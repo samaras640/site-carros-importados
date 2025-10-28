52993d286e8173d435ec5c9ae74e645da6bfc72b
samara.silva <samarasilvasantana20@gmail.com>
2025-10-28 15:36:31 -0300

    alteração

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("formConato").addEventListener("submi", function (event){
+event.preventDefault();
+const nome = document.getElementById ("nome").value;
+const email = document.getElementById ("email").value;
+alert('Obrigado, ${nome} ! sua mensagem foi enviada com sucesso para o email: $(email}.');
+document.getElementById("formContato").reset();
+});
+
+
+
+
